85c9d058a9856d44d22c6a9534832bf889bf6382
aonosomeday <180724451+aonosomeday@users.noreply.github.com>
2025-01-31 21:39:15 +0900

    add image to card in profile card

--- components/Card.tsx
+++ components/Card.tsx
@@ -1,9 +1,21 @@
-export default function Card({ href, title, description, img }: { href: string, title: string, description: string, img?: HTMLImageElement }) {
-  return(
-    <a href={href} className="w-full block mt-4 p-6 bg-white rounded-lg shadow-sm hover:bg-gray-100">
-      <h2 class="text-2xl font-bold">{title}</h2>
-      <p>{description}</p>
-      {img}
+export default function Card(
+  { href, title, description, imgPath, imgAlt }: {
+    href: string,
+    title: string,
+    description: string,
+    imgPath?: string,
+    imgAlt?: string
+  }) {
+  return (
+    <a href={href} className="w-full flex justify-between mt-4 p-6 bg-white rounded-lg shadow-sm hover:bg-gray-100">
+      <div className="">
+        <h2 className="text-2xl font-bold">{title}</h2>
+        <p>{description}</p>
+      </div>
+      {
+        (typeof imgPath !== "undefined") ?
+          <img className="rounded-full" height={55} width={55} alt={imgAlt} src={imgPath} /> : ""
+      }
     </a>
   );
 }
