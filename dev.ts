7cac71e1c75ee878543f95fcdc23f397e0759090
aonosomeday <180724451+aonosomeday@users.noreply.github.com>
2025-02-24 15:51:44 +0900

    apply posts when use dev.ts

--- dev.ts
+++ dev.ts
@@ -2,7 +2,10 @@
 
 import dev from "$fresh/dev.ts";
 import config from "./fresh.config.ts";
+import { applyPostsToDatabase } from "./utils/posts.ts";
 
 import "$std/dotenv/load.ts";
+
+await applyPostsToDatabase();
 
 await dev(import.meta.url, "./main.ts", config);
